7d5625e0266f22b92586cb564b7d79374c50d2b3
jillwilliams <codeGeek.03.14@gmail.com>
2019-01-23 18:29:57 -0700

    e-commerce app

--- src/components/Default.js
+++ src/components/Default.js
@@ -8,7 +8,7 @@ export default class Default extends Component {
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
-            <h1 className="display-3">404</h1>
+            <h1 className="display-4">404</h1>
             <h1>error</h1>
             <h2>page not found</h2>
             <h3>the requested URL
